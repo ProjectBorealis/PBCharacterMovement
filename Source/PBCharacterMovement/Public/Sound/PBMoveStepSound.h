b9e0a19b80222b793b9596d044e3f8d2bf1cc2f1
mastercoms <mastercoms@tuta.io>
2020-11-01 11:54:49 -0500

    import move step sound for public redist
    fixes a few issues with 4.25 people were having

--- Source/PBCharacterMovement/Public/Sound/PBMoveStepSound.h
+++ Source/PBCharacterMovement/Public/Sound/PBMoveStepSound.h
@@ -1,80 +1,81 @@
-// Copyright 2017-2019 Project Borealis
+// Copyright Project Borealis
 
 #pragma once
 
 #include "CoreMinimal.h"
 
+#include "Engine/EngineTypes.h"
+
 #include "PBMoveStepSound.generated.h"
 
 class USoundCue;
 /**
- * 
+ *
  */
 UCLASS(Blueprintable)
 class PBCHARACTERMOVEMENT_API UPBMoveStepSound : public UObject
 {
 	GENERATED_BODY()
 public:
-	UPBMoveStepSound();
-	~UPBMoveStepSound();
+	UFUNCTION()
+	TEnumAsByte<enum EPhysicalSurface> GetSurfaceMaterial() const { return SurfaceMaterial; }
 
 	UFUNCTION()
-	FORCEINLINE TEnumAsByte<enum EPhysicalSurface> GetSurfaceMaterial() const { return SurfaceMaterial;  }
+	TArray<USoundCue*> GetStepLeftSounds() const { return StepLeftSounds; }
 
 	UFUNCTION()
-	FORCEINLINE TArray<USoundCue*> GetStepLeftSounds() const { return StepLeftSounds;  }
+	TArray<USoundCue*> GetStepRightSounds() const { return StepRightSounds; }
 
 	UFUNCTION()
-	FORCEINLINE TArray<USoundCue*> GetStepRightSounds() const { return StepRightSounds; }
+	TArray<USoundCue*> GetSprintLeftSounds() const { return SprintLeftSounds; }
 
 	UFUNCTION()
-	FORCEINLINE TArray<USoundCue*> GetJumpSounds() const
-	{
-		return JumpSounds;
-	}
+	TArray<USoundCue*> GetSprintRightSounds() const { return SprintRightSounds; }
 
 	UFUNCTION()
-	FORCEINLINE TArray<USoundCue*> GetLandSounds() const
-	{
-		return LandSounds;
-	}
+	TArray<USoundCue*> GetJumpSounds() const { return JumpSounds; }
 
 	UFUNCTION()
-	FORCEINLINE float GetWalkVolume() const
-	{
-		return WalkVolume;
-	}
+	TArray<USoundCue*> GetLandSounds() const { return LandSounds; }
 
 	UFUNCTION()
-	FORCEINLINE float GetSprintVolume() const
-	{
-		return SprintVolume;
-	}
+	float GetWalkVolume() const { return WalkVolume; }
+
+	UFUNCTION()
+	float GetSprintVolume() const { return SprintVolume; }
 
 private:
 	/** The physical material associated with this move step sound */
-	UPROPERTY(EditDefaultsOnly)
-	TEnumAsByte<enum EPhysicalSurface> SurfaceMaterial;
+	UPROPERTY(EditDefaultsOnly, Category = Material)
+	TEnumAsByte<EPhysicalSurface> SurfaceMaterial;
 
 	/** The list of sounds to randomly choose from when stepping left */
-	UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(EditDefaultsOnly, Category = Sounds)
 	TArray<USoundCue*> StepLeftSounds;
 
 	/** The list of sounds to randomly choose from when stepping right */
-	UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(EditDefaultsOnly, Category = Sounds)
 	TArray<USoundCue*> StepRightSounds;
 
+	/** The list of sounds to randomly choose from when sprinting left */
+	UPROPERTY(EditDefaultsOnly, Category = Sounds)
+	TArray<USoundCue*> SprintLeftSounds;
+
+	/** The list of sounds to randomly choose from when sprinting right */
+	UPROPERTY(EditDefaultsOnly, Category = Sounds)
+	TArray<USoundCue*> SprintRightSounds;
+
 	/** The list of sounds to randomly choose from when jumping */
-	UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(EditDefaultsOnly, Category = Sounds)
 	TArray<USoundCue*> JumpSounds;
 
 	/** The list of sounds to randomly choose from when landing */
-	UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(EditDefaultsOnly, Category = Sounds)
 	TArray<USoundCue*> LandSounds;
 
-	UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(EditDefaultsOnly, Category = Volume)
 	float WalkVolume = 0.2f;
 
-	UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(EditDefaultsOnly, Category = Volume)
 	float SprintVolume = 0.5f;
 };
